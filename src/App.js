16ccfdfe94c2234a45f38b10cfb0cd70ecd1028b
Duc Anh <ducanh.le1908@gmail.com>
2022-08-30 15:08:53 +0700

    add color box useState

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
 import './App.scss';
+import ColorBox from './components/color-box';
 
 function App() {
   return (
     <div className="app">
-      <h1>Well come to</h1>
+      <h1>Well come to react </h1>
+      <ColorBox />
     </div>
   );
 }
---
+++ src/components/color-box/index.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import './color-box.scss'
+ColorBox.propTypes = {
+    
+};
+function getRamdomColor() {
+    const COLOR_LIST = ['deeppink', 'green', 'yellow', 'black', 'blue']
+    const randomIndex = Math.trunc(Math.random() * 5);
+    return COLOR_LIST[randomIndex];
+}
+
+function ColorBox() {
+    const [color, setColor] = useState(()=>{
+        const initColor = localStorage.getItem('box_color') || 'deeppink';
+        console.log(initColor);
+        return initColor;
+    });
+    function handdleBoxClick() {
+        const newColor =  getRamdomColor();
+        setColor(newColor);
+        localStorage.setItem('box_color', newColor)
+    }
+    return (
+        <div className='color-box' 
+        style={{backgroundColor: color}}
+        onClick={handdleBoxClick}
+        >
+           
+        </div>
+    );
+}
+
+export default ColorBox;
