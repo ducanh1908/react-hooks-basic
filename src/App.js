03a8c5c2dd54b7852dc29eba37128de044451688
Duc Anh <ducanh.le1908@gmail.com>
2022-08-30 15:55:23 +0700

    add todo list

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-
+import { useState } from 'react';
 import './App.scss';
-import ColorBox from './components/color-box';
+// import ColorBox from './components/color-box';
+import TodoList from './components/todo-list';
 
 function App() {
+  const initTodoList = [
+    { id: 1, title: 'I love Easy Frontend! 😍 ' },
+    { id: 2, title: 'We love Easy Frontend! 🥰 ' },
+    { id: 3, title: 'They love Easy Frontend! 🚀 ' },
+    ];
+  const [todoList, setTodoList] = useState(initTodoList);
+  function handleTodoClick(todo) {
+    console.log(todo);
+    const index = todoList.findIndex(x => x.id === todo.id);
+    if(index < 0) return ;
+    const newTodoList = [...todoList];
+    newTodoList.splice(index, 1);
+    setTodoList(newTodoList);
+  }
   return (
     <div className="app">
-      <h1>Well come to react </h1>
-      <ColorBox />
+      <h1>React Hooks - TodoList</h1>
+        <TodoList  todos={todoList} onTodoClick ={handleTodoClick}/>
     </div>
   );
 }
--- src/components/color-box/index.jsx
+++ src/components/color-box/index.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import PropTypes from 'prop-types';
 import './color-box.scss'
 ColorBox.propTypes = {
     
